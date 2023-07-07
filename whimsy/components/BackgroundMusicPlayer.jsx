940f20a5507de89d42795e669bd2a39fadfa6d6a
Alexa_Pettitt <91113965+AlexaPea@users.noreply.github.com>
2023-07-07 09:12:51 +0200

    fixed music player
    Here I made it so that the pause music functionality works correctly.

--- whimsy/components/BackgroundMusicPlayer.jsx
+++ whimsy/components/BackgroundMusicPlayer.jsx
@@ -5,55 +5,63 @@ import { Audio } from 'expo-av';
 import BackgroundMusic from '../assets/backgroundMusic.mp3';
 
 export default function BackgroundMusicPlayer() {
-  const [sound, setSound] = useState(null);
-  const [isPlaying, setIsPlaying] = useState(true);
-
-  useEffect(() => {
-    // Load the background music
-    const loadSound = async () => {
-      try {
-        const { sound } = await Audio.Sound.createAsync(BackgroundMusic, { isLooping: true });
-        setSound(sound);
-      } catch (error) {
-        console.log('Error loading sound:', error);
-      }
-    };
-
-    loadSound();
-
-    // Clean up resources when the component unmounts
-    return () => {
-      if (sound) {
-        sound.unloadAsync();
-      }
-    };
-  }, []);
-
-  useEffect(() => {
-    // Play or pause the background music based on the isPlaying state
-    const toggleMusic = async () => {
+    const [sound, setSound] = useState(null);
+    const [isPlaying, setIsPlaying] = useState(true);
+  
+    useEffect(() => {
+      // Load the background music
+      const loadSound = async () => {
+        try {
+          const { sound } = await Audio.Sound.createAsync(BackgroundMusic, { isLooping: true });
+          setSound(sound);
+        } catch (error) {
+          console.log('Error loading sound:', error);
+        }
+      };
+  
+      loadSound();
+  
+      // Clean up resources when the component unmounts
+      return () => {
+        if (sound) {
+          sound.unloadAsync();
+        }
+      };
+    }, []);
+  
+    useEffect(() => {
+      // Play the background music immediately if the sound is loaded
+      const playMusic = async () => {
+        try {
+          if (sound) {
+            await sound.playAsync();
+          }
+        } catch (error) {
+          console.log('Error playing sound:', error);
+        }
+      };
+  
+      playMusic();
+    }, [sound]);
+  
+    const togglePlayPause = async () => {
       if (sound) {
         if (isPlaying) {
-          await sound.playAsync();
-        } else {
           await sound.pauseAsync();
+        } else {
+          await sound.playAsync();
         }
       }
+  
+      setIsPlaying((prevState) => !prevState);
     };
-
-    toggleMusic();
-  }, [isPlaying]);
-
-  const togglePlayPause = () => {
-    setIsPlaying((prevState) => !prevState);
-  };
-
-  return (
-    <TouchableOpacity style={styles.musicPlayer} onPress={togglePlayPause}>
-      <Ionicons name={isPlaying ? 'volume-high-outline' : 'volume-mute-outline'} size={30} color="white" />
-    </TouchableOpacity>
-  );
-}
+  
+    return (
+      <TouchableOpacity style={styles.musicPlayer} onPress={togglePlayPause}>
+        <Ionicons name={isPlaying ? 'volume-high-outline' : 'volume-mute-outline'} size={30} color="white" />
+      </TouchableOpacity>
+    );
+  }
 
 const styles = {
   musicPlayer: {
@@ -63,7 +71,7 @@ const styles = {
     width: 60,
     height: 60,
     borderRadius: 55,
-    backgroundColor: 'rgba(68, 29, 12, 0.69)',
+    backgroundColor: 'rgba(244, 238, 229, 0.2)',
     
     alignItems: 'center',
     justifyContent: 'center',
--- whimsy/screens/HomeScreen.jsx
+++ whimsy/screens/HomeScreen.jsx
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, Text, View, ImageBackground, TouchableOpacity, Image } from 'react-native';
+import { StyleSheet, Text, View, ImageBackground, TouchableOpacity, Image, ScrollView } from 'react-native';
 import * as Font from 'expo-font';
 import { signOutUser } from '../services/firebaseAuth';
 import { getCurrentUser } from '../services/firebaseAuth';
@@ -10,6 +10,7 @@ const HomeScreen = ({ navigation }) => {
   const [fontLoaded, setFontLoaded] = useState(false);
   const [userRole, setUserRole] = useState(null);
   const user = getCurrentUser();
+  const [countdown, setCountdown] = useState('');
 
   const loadFonts = async () => {
     await Font.loadAsync({
@@ -36,6 +37,43 @@ const HomeScreen = ({ navigation }) => {
     loadFonts();
   }, []);
 
+  useEffect(() => {
+    const calculateCountdown = () => {
+      const today = new Date();
+      const currentMonth = today.getMonth();
+      const currentYear = today.getFullYear();
+
+      // Calculate the next month and year
+      let nextMonth = currentMonth + 1;
+      let nextYear = currentYear;
+      if (nextMonth === 12) {
+        nextMonth = 0;
+        nextYear++;
+      }
+
+      const nextMonthDate = new Date(nextYear, nextMonth, 1);
+      const timeRemaining = nextMonthDate.getTime() - today.getTime();
+
+      // Convert milliseconds to days, hours, minutes, and seconds
+      const days = Math.floor(timeRemaining / (1000 * 60 * 60 * 24));
+      const hours = Math.floor((timeRemaining % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+      const minutes = Math.floor((timeRemaining % (1000 * 60 * 60)) / (1000 * 60));
+      const seconds = Math.floor((timeRemaining % (1000 * 60)) / 1000);
+
+      const countdownString = `${days}d ${hours}h ${minutes}m ${seconds}s`;
+
+      setCountdown(countdownString);
+    };
+
+    calculateCountdown();
+
+    const timer = setInterval(calculateCountdown, 1000);
+
+    return () => {
+      clearInterval(timer);
+    };
+  }, []);
+
   const navigateToOwnStoryScreen = () => {
     navigation.navigate('Write');
   };
@@ -49,44 +87,62 @@ const HomeScreen = ({ navigation }) => {
   };
 
   return (
-    <ImageBackground
-      source={require('../assets/bg/home.png')}
-      style={styles.backgroundImage}
-    >
-      {fontLoaded ? (
-        <>
-          <Text style={styles.heading}>Hey {user.displayName}!</Text>
-          <Text style={styles.body}>Our favourite {userRole}.</Text>
+    <ScrollView contentContainerStyle={styles.scrollContainer}>
+      <ImageBackground
+        source={require('../assets/bg/home.png')}
+        style={styles.backgroundImage}
+      >
+        {fontLoaded ? (
+          <>
+            <Text style={styles.heading}>Hey {user.displayName}!</Text>
+            <Text style={styles.body}>Our favourite {userRole}.</Text>
 
-          <BackgroundMusicPlayer />
+            <BackgroundMusicPlayer />
 
-          <TouchableOpacity style={styles.logoutButton} onPress={signOutUser}>
-            <Image source={require('../assets/logout-icon.png')} style={styles.logoutIcon} />
-          </TouchableOpacity>
-
-          {userRole === 'creator' ? (
-            <TouchableOpacity style={styles.feather} onPress={navigateToOwnStoryScreen}>
-              <Image source={require('../assets/feather.png')} style={styles.featherImg} />
+            <TouchableOpacity style={styles.logoutButton} onPress={signOutUser}>
+              <Image source={require('../assets/logout-icon.png')} style={styles.logoutIcon} />
             </TouchableOpacity>
-          ) : userRole === 'judge' ? (
-            <TouchableOpacity style={styles.gavel} onPress={navigateToJudgeScreen}>
-              <Image source={require('../assets/gavel.png')} style={styles.gavelImg} />
-            </TouchableOpacity>
-          ) : null}
 
-          <TouchableOpacity style={styles.glass} onPress={navigateToLibraryScreen}>
-            <Image source={require('../assets/glass.png')} style={styles.glassImg} />
-          </TouchableOpacity>
-        </>
-      ) : null}
-    </ImageBackground>
+            {userRole === 'creator' ? (
+              <TouchableOpacity style={styles.feather} onPress={navigateToOwnStoryScreen}>
+                <Image source={require('../assets/feather.png')} style={styles.featherImg} />
+              </TouchableOpacity>
+            ) : userRole === 'judge' ? (
+              <TouchableOpacity style={styles.gavel} onPress={navigateToJudgeScreen}>
+                <Image source={require('../assets/gavel.png')} style={styles.gavelImg} />
+              </TouchableOpacity>
+            ) : null}
+
+            <TouchableOpacity style={styles.glass} onPress={navigateToLibraryScreen}>
+              <Image source={require('../assets/glass.png')} style={styles.glassImg} />
+            </TouchableOpacity>
+
+            {/* Display the countdown timer */}
+            <View style={styles.timerContainer}>
+            <View style={styles.timerContainerStyle}>
+              <Text style={styles.CountText}>CountDown</Text>
+              <Text style={styles.compText}>Next Competition</Text>
+              <Text style={styles.timerText}>{countdown}</Text>
+              </View>
+              <Text style={styles.paragraph}>Each month our we clear our library and make room for new masterpieces.</Text>
+            </View>
+
+           
+          </>
+        ) : null}
+      </ImageBackground>
+    </ScrollView>
   );
 };
 
 const styles = StyleSheet.create({
+  scrollContainer: {
+    flexGrow: 1,
+    height: 1250
+  },
   backgroundImage: {
     flex: 1,
-    resizeMode: 'cover',
+    // resizeMode: '',
     paddingTop: 0,
     paddingHorizontal: 30,
   },
@@ -117,7 +173,7 @@ const styles = StyleSheet.create({
     width: 40,
   },
   feather: {
-    marginTop: 600,
+    marginTop: 690,
     marginLeft: 30,
     position: 'absolute',
   },
@@ -126,7 +182,7 @@ const styles = StyleSheet.create({
     height: 118,
   },
   glass: {
-    marginTop: 620,
+    marginTop: 720,
     marginLeft: 220,
     position: 'absolute',
   },
@@ -135,7 +191,7 @@ const styles = StyleSheet.create({
     height: 144,
   },
   gavel: {
-    marginTop: 600,
+    marginTop: 650,
     marginLeft: 60,
     position: 'absolute',
   },
@@ -143,6 +199,50 @@ const styles = StyleSheet.create({
     width: 160,
     height: 124,
   },
+  timerContainer: {
+    position: 'absolute',
+    top: 850,
+    paddingTop: 20,
+    alignSelf: 'flex-start',
+    paddingRight: 30,
+    paddingBottom: 10,
+
+ 
+  },
+  timerContainerStyle: {
+   
+    paddingTop: 20,
+   width: 350,
+    paddingLeft: 40,
+    paddingRight: 30,
+    paddingBottom: 30,
+    backgroundColor: 'rgba(244, 238, 229, 0.2)',
+ 
+  },
+  timerText: {
+    color: 'white',
+    fontSize: 32,
+    fontWeight: 300
+  },
+  CountText:{
+    fontSize: 16,
+    fontWeight: 400,
+    color: 'white',
+   
+  },
+  compText:{
+    fontFamily: 'Hensa',
+    fontSize: 32,
+    color: 'white',
+    paddingTop: 10,
+    // lineHeight: 50,
+  },
+  paragraph:{
+    fontSize: 16,
+    color: 'white',
+    paddingTop: 20,
+    paddingLeft: 40
+  }
 });
 
 export default HomeScreen;
